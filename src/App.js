a220619d15f2cdd4a235f4f13c06d2f4ba0922c5
jorgequinterodoria <69540195+jorgequinterodoria@users.noreply.github.com>
2022-08-29 14:49:34 -0500

    Mach Eight Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, TextField } from '@mui/material';
+import { useState } from 'react';
+
 
 function App() {
+  const [numbers, setNumbers] = useState([])
+  const [value, setValue] = useState()
+  const execute = () => {
+    const myArray = numbers.split(',').map(Number)
+    for (let i = 0; i < myArray.length; i++) {
+      for (let j = i + 1; j < myArray.length; j++) {
+        let answer = myArray[i] + myArray[j]
+        if (answer === parseInt(value)) {
+          var targetDiv = document.getElementById('results');
+          targetDiv.innerHTML += `${myArray[i]} , ${myArray[j]}<br/>`
+        }
+      }
+    }
+    setNumbers([])
+    setValue(0)
+  }
   return (
     <div className="App">
+      <h2>Mach Eight</h2>
+      <p>The task is to write a function that finds pairs of integers from a list that
+        sum to a given value. The function will take as input the list of numbers as
+        well as the target sum.</p>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <TextField
+          id="outlined-basic"
+          label="Write the list "
+          variant="outlined"
+          helperText="Enter integer values, separated by commas"
+          onChange={e => { setNumbers(e.target.value); }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Enter the value"
+          variant="outlined"
+          helperText="Enter the target num"
+          onChange={e => setValue(e.target.value)}
+        />
+        <Button
+          type="submit"
+          onClick={execute}
+          variant='contained'
         >
-          Learn React
-        </a>
+          Execute
+        </Button>
       </header>
+      <div id='results'>
+        <h3>Results</h3>
+      </div>
     </div>
   );
 }
